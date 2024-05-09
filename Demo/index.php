a5ae45e11cd96215434cf6a37c25de50d3d775fe
Tushar Bhatt <121217836+tushar29blank@users.noreply.github.com>
2024-05-09 18:04:21 +0530

    Create index.php

---
+++ Demo/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body class="c2">
+    
+        <h1 class="c3">Welcome to this page...</h1>
+        <p class="c4">if you are new please... <a style="color:violet" href='signuppage.php'>SIGN_IN</a>
+        <br><br>Or <br><br>
+        if you are visiting again OR have you already signed in... <a style="color:violet" href='loginpage.php'>LOGIN</a>
+    
+</body>
+</html>
